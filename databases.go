cb2062fcd298db3b609c5cf5cc41175bfd2d27d9
Pei-Ming Wu <p408865@gmail.com>
2021-05-16 00:45:22 +0800

    refactor: move Color to types.go

--- databases.go
+++ databases.go
@@ -259,30 +259,6 @@ func (d *Database) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
-type Color string
-
-const (
-	DefaultColor          Color = "default"
-	GrayColor             Color = "gray"
-	BrownColor            Color = "brown"
-	OrangeColor           Color = "orange"
-	YellowColor           Color = "yellow"
-	GreenColor            Color = "green"
-	BlueColor             Color = "blue"
-	PurpleColor           Color = "purple"
-	PinkColor             Color = "pink"
-	RedColor              Color = "red"
-	GrayBackgroundColor   Color = "gray_background"
-	BrownBackgroundColor  Color = "brown_background"
-	OrangeBackgroundColor Color = "orange_background"
-	YellowBackgroundColor Color = "yellow_background"
-	GreenBackgroundColor  Color = "green_background"
-	BlueBackgroundColor   Color = "blue_background"
-	PurpleBackgroundColor Color = "purple_background"
-	PinkBackgroundColor   Color = "pink_background"
-	RedBackgroundColor    Color = "red_background"
-)
-
 type Annotations struct {
 	// Whether the text is **bolded**.
 	Bold bool `json:"bold"`
--- types.go
+++ types.go
@@ -13,3 +13,27 @@ type PaginatedList struct {
 	HasMore    bool   `json:"has_more"`
 	NextCursor string `json:"next_cursor"`
 }
+
+type Color string
+
+const (
+	DefaultColor          Color = "default"
+	GrayColor             Color = "gray"
+	BrownColor            Color = "brown"
+	OrangeColor           Color = "orange"
+	YellowColor           Color = "yellow"
+	GreenColor            Color = "green"
+	BlueColor             Color = "blue"
+	PurpleColor           Color = "purple"
+	PinkColor             Color = "pink"
+	RedColor              Color = "red"
+	GrayBackgroundColor   Color = "gray_background"
+	BrownBackgroundColor  Color = "brown_background"
+	OrangeBackgroundColor Color = "orange_background"
+	YellowBackgroundColor Color = "yellow_background"
+	GreenBackgroundColor  Color = "green_background"
+	BlueBackgroundColor   Color = "blue_background"
+	PurpleBackgroundColor Color = "purple_background"
+	PinkBackgroundColor   Color = "pink_background"
+	RedBackgroundColor    Color = "red_background"
+)
