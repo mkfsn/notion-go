860d08b7b0ca7915ac5918f633559202b28780a6
Pei-Ming Wu <mkfsn@users.noreply.github.com>
2021-05-19 00:31:51 +0800

    test: HTTPError (#7)

--- errors.go
+++ errors.go
@@ -9,9 +9,26 @@ var (
 	ErrUnknown = errors.New("unknown")
 )
 
+// ErrorCode https://developers.notion.com/reference/errors
+type ErrorCode string
+
+const (
+	ErrorCodeInvalidJSON         ErrorCode = "invalid_json"
+	ErrorCodeInvalidRequestURI   ErrorCode = "invalid_request_url"
+	ErrorCodeInvalidRequest      ErrorCode = "invalid_request"
+	ErrorCodeValidationError     ErrorCode = "validation_error"
+	ErrorCodeUnauthorized        ErrorCode = "unauthorized"
+	ErrorCodeRestrictedResource  ErrorCode = "restricted_resource"
+	ErrorCodeObjectNotFound      ErrorCode = "object_not_found"
+	ErrorCodeConflictError       ErrorCode = "conflict_error"
+	ErrorCodeRateLimited         ErrorCode = "rate_limited"
+	ErrorCodeInternalServerError ErrorCode = "internal_server_error"
+	ErrorCodeServiceUnavailable  ErrorCode = "service_unavailable"
+)
+
 type HTTPError struct {
-	Code    string `json:"code"`
-	Message string `json:"message"`
+	Code    ErrorCode `json:"code"`
+	Message string    `json:"message"`
 }
 
 func (e HTTPError) Error() string {
